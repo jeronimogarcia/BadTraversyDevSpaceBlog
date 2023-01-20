146e2311a148d3f96a84397c010bf2dbc0b86d98
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-20 15:31:57 +0100

    getPosts function to clean up code

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -3,7 +3,8 @@ import path from "path";
 import matter from "gray-matter";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils";
+
+import { getPosts } from "@/lib/posts";
 
 export default function CategoryPage({ posts, categoryName }) {
   return (
@@ -46,21 +47,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name } }) {
   const files = fs.readdirSync(path.join("posts"));
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts()
 
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
@@ -68,7 +55,7 @@ export async function getStaticProps({ params: { category_name } }) {
 
   return {
     props: {
-      posts: categoryPosts.sort(sortByDate),
+      posts: categoryPosts,
       categoryName: category_name
     },
   };
